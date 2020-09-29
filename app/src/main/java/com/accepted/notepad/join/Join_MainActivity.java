73156a8ee3c6149355eb87eed2a96591da75384c
logia7987 <ansrjsdn7@naver.com>
2020-09-29 13:15:16 +0900

    뒤로가기 이벤트 추가

--- app/src/main/java/com/accepted/notepad/join/Join_MainActivity.java
+++ app/src/main/java/com/accepted/notepad/join/Join_MainActivity.java
@@ -40,6 +40,9 @@ public class Join_MainActivity extends AppCompatActivity {
     int certNum;
     boolean isChecked = false;
 
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long backPressedTime = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -184,6 +187,22 @@ public class Join_MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
+
+            finish();
+            super.onBackPressed();
+
+        } else {
+            backPressedTime = tempTime;
+            Toast.makeText(context, "뒤로가기를 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void hideKeyboard(View view) {
