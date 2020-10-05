a4656250d7b3bc6c9abd0b11e438c602a00ea990
Kwonhong <mkh9012@kookmin.ac.kr>
2020-10-05 16:21:39 +0900

    오류 수정

--- app/src/main/java/com/accepted/notepad/join/Join_MainActivity.java
+++ app/src/main/java/com/accepted/notepad/join/Join_MainActivity.java
@@ -128,7 +128,7 @@ public class Join_MainActivity extends AppCompatActivity {
                 } else {
                     InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
                     inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-                    sendSMS();
+                    checkDuplicationPhone();
                 }
             }
             });
@@ -145,6 +145,10 @@ public class Join_MainActivity extends AppCompatActivity {
                     if (inputNum.equals(String.valueOf(certNum))) {
                         isChecked = true;
                         Toast.makeText(context,"인증되었습니다.",Toast.LENGTH_SHORT).show();
+                        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+                        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    } else {
+                        Toast.makeText(context,"인증 번호를 확인해주세요",Toast.LENGTH_SHORT).show();
                         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
                         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
                     }
@@ -197,6 +201,39 @@ public class Join_MainActivity extends AppCompatActivity {
     public void hideKeyboard(View view) {
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public void checkDuplicationPhone() {
+        String PhoneNum = (((EditText)findViewById(R.id.et_phone_join)).getText()).toString();
+
+
+        RequestQueue postRequestQueue = VolleySingleton.getInstance(context).getRequestQueue();
+        StringRequest postJsonRequest = new StringRequest(Request.Method.POST, SaveSharedPreference.getServerIp() + "/Member/checkDuplicationPhone.do", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    String result = obj.getString("result");
+
+                    if(result.equals("success")) {
+                        sendSMS();
+                    } else {
+                        Toast.makeText(context,"이미 가입한 휴대폰 번호입니다.",Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, SaveSharedPreference.getErrorListener(context)) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("Phone", PhoneNum);
+                return params;
+            }
+        };
+        postRequestQueue.add(postJsonRequest);
     }
 
     public void sendSMS() {
